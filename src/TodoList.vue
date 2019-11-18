192f5f1387ca7d000ff3485a6aac37380ff6cde7
kuranado <jingxinling1024@163.com>
2019-11-18 13:35:18 +0800

    使用 vue-cli 脚手架搭建的项目结构，编写简单 todolist

---
+++ src/TodoList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <label>
+      <input v-model="inputValue"/>
+    </label>
+    <button @click="handleClick">添加</button>
+    <ul>
+      <todo-item v-for="(item, index) of list" :key="index" :content="item" :index="index" @delete="handleDelete"></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+export default {
+  data: function () {
+    return {
+      inputValue: '',
+      list: []
+    }
+  },
+  components: {
+    'todo-item': TodoItem
+  },
+  methods: {
+    handleClick: function () {
+      if (this.inputValue === '') {
+        return
+      }
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handleDelete: function (index) {
+      this.list.splice(index, 1)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
